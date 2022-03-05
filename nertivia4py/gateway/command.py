55d7b8bcddac0bd7b78fe96a30e5c0d8f2822274
Ben Tettmar <bentettmar@yahoo.com>
2022-03-05 17:40:05 +0000

    Added description, aliases and usage to commands.

--- nertivia4py/gateway/command.py
+++ nertivia4py/gateway/command.py
@@ -1,6 +1,9 @@
 class Command:
-    def __init__(self, name: str, callback: callable) -> None:
+    def __init__(self, name: str, description: str, usage: str, aliases: list, callback: callable) -> None:
         self.name = name
+        self.description = description
+        self.aliases = aliases
+        self.usage = usage
         self.callback = callback
 
     def get_callback(self) -> callable:
