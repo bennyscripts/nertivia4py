303f7717afd826db4aaa64f0da02bbc5618b87b0
Ben Tettmar <bentettmar@yahoo.com>
2022-03-05 17:39:43 +0000

    Cleaned up client class and made it more pythonic.

--- nertivia4py/gateway/client.py
+++ nertivia4py/gateway/client.py
@@ -1,75 +1,71 @@
-import requests
 import socketio
-import asyncio
-import importlib
-import traceback
-import base64
-import os
 import shlex
-import datetime
-import inspect
 
-from .command import Command
-from .events import Events
+from . import events
+from . import command
 
-from ..extra import Extra
-from ..user import User
-from ..message import Message
 from .. import nertivia
+from .. import extra
+from .. import message
 
 class Client:
     def __init__(self, command_prefix, debug=False):
-        self.token = ""
         self.socket = socketio.Client(engineio_logger=False, logger=debug)
-        self.socketIp = "https://nertivia.net/"
-        self.headers = {"Authorization": self.token, "content-type": "application/json"}
-        self.user = ""
-        self.start_time = datetime.datetime.now()
+        self.socket_ip = "https://nertivia.net/"
+
         self.command_prefix = command_prefix
         self.commands = []
 
-        Extra.setauthtoken(self.token)    
+        self.user = None
+        self.token = ""
+
+        extra.Extra.setauthtoken(self.token)
 
     def run(self, token):
+        extra.Extra.setauthtoken(token)
+
         self.token = token
-        # self.user = User(response.json()["user"]["id"], response.json()["user"]["username"], response.json()["user"]["tag"], response.json()["user"]["avatar"], response.json()["user"]["banner"], response.json()["user"]["created"])
         self.user = nertivia.Nertivia(token)
-        Extra.setauthtoken(token)
-        self.socket.connect(self.socketIp, namespaces=["/"], transports=["websocket"])
+
+        self.socket.connect(self.socket_ip, namespaces=["/"], transports=["websocket"])
         self.socket.emit("authentication", {"token": token})
         self.socket.wait()
-        self.start_time = datetime.datetime.now()
-
-    def parse_message(self, message):
-        command = message.content.replace(self.command_prefix, "")
-        args = shlex.split(command)
-        command = args[0]
-        args.pop(0)
-
-        return command, args
 
     def _command_event_handler(self, event):
-        message = Message(event["message"]["messageID"], event["message"]["channelId"])
+        msg = message.Message(event["message"]["messageID"], event["message"]["channelId"])
 
-        if message.content.startswith(self.command_prefix):
-            cmd, args = self.parse_message(message)
-            for command in self.commands:
-                if command.name == cmd:
-                    callback = command.get_callback()
-                    callback(message, args)
+        if msg.content.startswith(self.command_prefix):
+            command = msg.content.replace(self.command_prefix, "")
+            args = shlex.split(command)
+            command = args[0]
+            args.pop(0)
 
-    def register_command(self, name: str, callback: callable):
-        self.commands.append(Command(name, callback))
-
-        self.socket.on("message:created", self._command_event_handler)
+            for cmd in self.commands:
+                if cmd.name == command or command in cmd.aliases:
+                    callback = cmd.get_callback()
+                    callback(msg, args)
 
     def event(self, *args):
         eventname = args[0].__name__
 
-        events = Events().events
+        evnts = events.Events().events
 
-        for event in events:
+        for event in evnts:
             for key, value in event.items():
                 if key == eventname:
                     self.socket.on(value, args[0])
-        
+
+    def command(self, **kwargs):
+        def decorator(func):
+            command_name = kwargs["name"] if "name" in kwargs else func.__name__
+            command_description = kwargs["description"] if "description" in kwargs else "No description provided."
+            command_usage = kwargs["usage"] if "usage" in kwargs else ""
+            command_aliases = kwargs["aliases"] if "aliases" in kwargs else []
+
+            command_callback = func
+
+            self.commands.append(command.Command(command_name, command_description, command_usage, command_aliases, command_callback))
+            self.socket.on("message:created", self._command_event_handler)
+
+            return func
+        return decorator
