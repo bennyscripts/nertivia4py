39ec77ba6a9fe70dcbd899c09ad913ba675dec70
Ben Tettmar <bentettmar@yahoo.com>
2022-03-05 18:50:43 +0000

    Added description about the lib.

--- nertivia4py/__init__.py
+++ nertivia4py/__init__.py
@@ -1,3 +1,11 @@
+"""
+# Nertivia4PY
+A Python wrapper for the Nertivia API.  
+
+Checkout the Github for examples and documentation.  
+https://github.com/bentettmar/nertivia4py
+"""
+
 from .nertivia import *
 from .user import *
 from .bot import *
