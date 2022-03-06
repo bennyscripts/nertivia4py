9db2ff4c1b2ad7199db65b248c45bb5d6f73a6cc
Ben Tettmar <bentettmar@yahoo.com>
2022-03-06 12:03:39 +0000

    Change readme to markdown version.

--- setup.py
+++ setup.py
@@ -1,52 +1,42 @@
 from distutils.core import setup
 
 readme = """
-Nertivia4PY
-===========
+# Nertivia4PY
+A Python wrapper for the Nertivia API.  
+Support Nertivia server : https://nertivia.net/i/nertivia4py
 
-| A Python wrapper for the Nertivia API.
-| Support Nertivia server : https://nertivia.net/i/nertivia4py
+> ### Install
+> ```
+> pip install nertivia4py
+> ```
 
-   .. rubric:: Install
-      :name: install
-
-   ::
-
-      pip install nertivia4py
-
-..
-
-   .. rubric:: Example
-      :name: example
-
-   .. code:: python
-
-      import nertivia4py
-
-      token = "TOKEN_HERE"
-      prefix = "!"
-
-      bot = nertivia4py.Bot(prefix)
-
-      @bot.event
-      def on_success(event):
-          print("Connected!")
-
-      @bot.command(name="ping", description="Ping command.")
-      def ping_command(message, args):
-          message.reply("Pong!")
-
-      bot.run(token)
-
-   For more examples, take a look at the examples folder in the github
-   repo.
-
+> ### Example 
+> ```python
+> import nertivia4py
+> 
+> token = "TOKEN_HERE"
+> prefix = "!"
+> 
+> bot = nertivia4py.Bot(prefix)
+> 
+> @bot.event
+> def on_success(event):
+>     print("Connected!")
+> 
+> @bot.command(name="ping", description="Ping command.")
+> def ping_command(message, args):
+>     message.reply("Pong!")
+> 
+> bot.run(token)
+> ```  
+> 
+> For more examples, take a look at the examples folder in the github repo.
 """
 
 setup(
     name='nertivia4py',
-    packages=['nertivia4py', 'nertivia4py.gateway'],
-    version='0.6',
+    packages=['nertivia4py', 'nertivia4py.gateway', 'nertivia4py.utils', 'nertivia4py.commands'],
+    version='1.0.0',
     license='MIT',
     description='A Python wrapper for the Nertivia API',
     long_description_content_type="text/markdown",
